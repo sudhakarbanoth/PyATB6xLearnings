d04d2aaa0a6fa4dfd5ef77914af4f1dd8c5c2c55
Sudhakar Banoth <sudhakar.banoth529@gmail.com>
2025-12-11 12:30:03 +0530

    Initial commit

---
+++ src/ex_02_Keywords_Identifier_Variables/Lab005_Variable_Part1.py
@@ -0,0 +1,12 @@
+age = 30
+age = "Sudhakar"
+# Dynamic Typed Language. - It can understand what you are doing and what you want.
+
+pi = 3.14
+name = "Sudhakar"
+name = "Sudhakar"
+name = "Sudhakar"
+name = "Sudhakar"
+name = "Banoth"
+print(name) # Stores the latest value
+
---
+++ src/ex_02_Keywords_Identifier_Variables/Lab006_Identifier.py
@@ -0,0 +1,16 @@
+# Identifier -variable names
+age = 65
+_age = 65
+
+print(age)
+print(_age)
+
+# _ can be a variable name
+_ = 12
+print(_)
+
+_ = _+1
+print(_)
+
+# number cannot be a variable
+# Alpha Numeric is possible
