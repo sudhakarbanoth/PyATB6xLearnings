765a4c35cbf7ad9c7b445bfb33fa889687c14668
Sudhakar Banoth <sudhakar.banoth529@gmail.com>
2026-01-31 23:12:25 +0530

    Committing Tasks for (16-Oct-2025)

---
+++ Task/Task005.py
@@ -0,0 +1,15 @@
+# In automation, you often compare expected and actual outputs.
+# Write code to check if a test case passed or failed.
+#
+# expected_title = "Dashboard"
+# actual_title = "Dashboard "
+#
+# ✅ Test Passed – Title matches
+
+expected_title = "Dashboard"
+actual_title = "Dashboard "
+
+if expected_title.strip().lower() == actual_title.strip().lower():
+    print("Test Passed")
+else:
+    print("Test Failed")
---
+++ Task/Task006.py
@@ -0,0 +1,15 @@
+# You want to check whether a web page loads within 3 seconds (performance test condition).
+#
+# load_time <=3.0 seconds
+# ✅ Page loaded successfully in {load_time} seconds
+# load_time = 4.2
+# ⚠️ Page load too slow: 4.2 seconds
+
+load_time = float(input("Enter the load time: "))
+
+
+# Performance threshold (3 seconds)
+if load_time <= 3:
+    print(f"✅ Page loaded successfully in {load_time} seconds")
+else:
+    print(f"⚠️ Page load too slow: {load_time} seconds")
