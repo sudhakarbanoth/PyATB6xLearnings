a79d563e223089e58041e685f9454aa5570d248e
Sudhakar Banoth <sudhakar.banoth529@gmail.com>
2026-01-31 23:15:43 +0530

    Committing Tasks for (16-Oct-2025)

--- Task/Task007.py
+++ Task/Task007.py
@@ -17,4 +17,4 @@ password = input("Enter Password: ")
 if username == "admin" and password == "1234":
     print("✅ Login Successful")
 else:
-    print("❌ Invalid Credentials")
+    print("❌ Invalid Credentials") 
