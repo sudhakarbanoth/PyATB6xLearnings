32055d13ac5a7bc38f6b76d041d3d42167d11d1d
Sudhakar Banoth <sudhakar.banoth529@gmail.com>
2026-02-02 23:22:38 +0530

    Committing while Loop  (18-Oct-2025)

---
+++ src/ex_07_Loops/While/Lab053_While.py
@@ -0,0 +1,15 @@
+# I
+# C
+# U
+
+i = 0 # Initialization
+while i <= 10: # Condition
+    print(i)
+    i = i + 1 #Updation
+
+
+# 10 is not included
+i = 0 # Initialization
+while i < 10: # Condition
+    print(i)
+    i = i + 1 #Updation
